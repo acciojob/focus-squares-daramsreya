a44ab35becfe1722f2419bd560894d5b2e5f7acb
daramsreya83@gmail.com <your.email@example.com>
2025-08-21 21:11:19 +0530

    Assigment

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //your JS code here. If required.
     const squares = document.querySelectorAll('.square');
 
-    squares.forEach((sq) => {
-      sq.addEventListener('mouseenter', () => {
-        squares.forEach(other => {
-          if (other !== sq) {
-            other.style.backgroundColor = "#6F4E37"; // Coffee
-          }
-        });
-      });
+		squares.forEach((sq) => {
+		  sq.addEventListener('mouseover', () => {
+		    squares.forEach(other => {
+		      if (other !== sq) {
+		        other.classList.add("coffee");
+		      }
+		    });
+		  });
 
-      sq.addEventListener('mouseleave', () => {
-        squares.forEach(other => {
-          other.style.backgroundColor = "#E6E6FA"; // Lavender
-        });
-      });
-    });
+		  sq.addEventListener('mouseout', () => {
+		    squares.forEach(other => {
+		      other.classList.remove("coffee");
+		    });
+		  });
+		});
